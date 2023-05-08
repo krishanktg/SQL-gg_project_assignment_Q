37fd4801c8fe6ffc616bc661b26778885fa883b3
Krishan Kumar <93551208+krishanktg@users.noreply.github.com>
2023-05-08 17:14:20 +0530

    Update gg_Assignmt_Q2.sql

--- gg_Assignmt_Q2.sql
+++ gg_Assignmt_Q2.sql
@@ -3,21 +3,23 @@ with t1 as
 (select us.app_id, uo.user_id,count(uo.offer_id) as total from q2_user_offer_data as uo
 JOIN q2_users_signup as us on uo.user_id= us.user_id
 GROUP BY user_id)
-SELECT app_id, sum(total) as num_offers_initiated
+SELECT app_id, sum(total) as Total_offers_initiated
 from t1
 GROUP BY app_id ;
 
 -- Offer Completion by users
 with t2 as
-(select user_id 
+(select user_id, count(offer_id) as total
 from q2_user_offer_data 
 where status = "completed" 
   GROUP BY user_id
 )
-SELECT app_id, count(*)
- from q2_users_signup
- where user_id in (SELECT * from t2)
- GROUP BY 1;
+
+SELECT app_id, sum(total) as Total_Offers_Completion_by_users
+ from q2_users_signup as us
+ join t2 on us.user_id = t2.user_id
+ GROUP BY 1
+;
  
 -- Rewards earned by users and Revenue generated
 
